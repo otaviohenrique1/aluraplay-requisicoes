c033645626f5bbb0c812755f641830bd1d35d02b
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-14 23:02:31 -0300

    commit 3

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -4,6 +4,21 @@ async function listaVideos() {
   return conexaoConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch("http://localhost:3000/videos",{
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      titulo: titulo,
+      descricao: `${descricao} mil visualizações`,
+      url: url,
+      imagem: imagem,
+    })
+  });
+}
+
 export const conectaApi = {
   listaVideos
 };
