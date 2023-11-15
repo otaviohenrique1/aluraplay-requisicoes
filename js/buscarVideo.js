dc084c1cba6df40129ef68d4c384a2e22b49e624
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-15 18:01:34 -0300

    commit 6

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -11,6 +11,9 @@ async function buscaVideo(evento) {
     lista.removeChild(lista.firstChild);
   }
   busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
+  if (busca.length == 0) {
+    lista.innerHTML = `<h2 class="mensagem__titulo">Não existem vídeos com esse termo</h2>`;
+  }
 }
 
 const botaoDePesquisa = document.querySelector("data-botao-pesquisa");
